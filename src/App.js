bba4169a4fdd01928f91545775c915b70ffa7986
karoprdev <karolaypasion@gmail.com>
2024-02-02 00:56:33 -0500

    upload api-los-simpsons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import simpsonsImage from './img/simpsons.png';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const getCharacters = async () => {
+    try {
+      const apiCharacters = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=50');
+      const jsonCharacters = await apiCharacters.json();
+
+      let charactersMap = jsonCharacters.map(item => {
+        return [item.character, item]
+      });
+      let charactersMapArr = new Map(charactersMap); //Pares de clave y valor
+
+      let uniqueCharacters = [...charactersMapArr.values()]; //Conversión a un array
+
+      setCharacters(uniqueCharacters);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Los simpsons</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={simpsonsImage} alt="Los simpsons" className="img-home" />
+            <button className="btn" onClick={getCharacters}>
+              Buscar personajes
+            </button>
+          </>
+        )}        
       </header>
     </div>
   );
